3d90f704fe3d751ebe09cffac304c50803e991d3
hexa-hedron <hwr.bluebird@gmail.com>
2020-11-25 20:48:08 +0900

    update

--- private/dao.php
+++ private/dao.php
@@ -36,6 +36,11 @@ class dao{
         $sql->bindValue(1, $task_id, PDO::PARAM_INT);
         $sql->execute();
 
+        $sqlStone = $this->$pdo->prepare("UPDATE users SET stone = stone+(SELECT stone_reward FROM tasks WHERE task_id = ?) WHERE user_id = ?")
+        $sqlStone->bindValue(1, $task_id, PDO::PARAM_INT);
+        $sqlStone->bindValue(2, $user_id, PDO::PARAM_INT);
+        $sqlStone->execute();
+
         return;
     }
 
